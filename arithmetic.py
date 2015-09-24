0237021bd7e53e45c773d264380699db08915194
Hackbright Student <info@hackbrightacademy.com>
2015-09-23 17:58:06 -0700

    arithmetic commit

---
+++ arithmetic.py
@@ -0,0 +1,31 @@
+def add(num1, num2):
+    summation = num1 + num2
+    return sum
+
+def subtract(num1, num2):
+    difference = num2 - num1
+    return difference
+
+def multiply(num1, num2):
+    product = num1 * num2
+    return product
+
+def divide(num1, num2):
+    quotient = float(num1/num2)
+    return quotient
+
+def square(num1):
+    squared = num1 ** 2
+    return squared
+
+def cube(num1):
+    cubed = num1 ** 3
+    return cubed
+
+def power(num1, num2):
+    powered = num1 ** num2
+    return powered
+
+def moded(num1, num2):
+    moded = num1 % num2
+    return moded
